7ca26da351f56007ed1dbe49ac0c36ca5bc8b190
Baikadamov <maxat.baikadamov@narxoz.kz>
2023-11-15 11:32:35 +0600

    add login page

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
 import Home from "./components/Home/home";
+import Registration from "./pages/registration";
+import Login from "./pages/login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/registration" element={<Registration/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
